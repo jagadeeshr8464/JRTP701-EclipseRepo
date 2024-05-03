a218ef82aef4746b09b504d639b4700b03b73e0d
jagadeesh <jagadeesh@gmail.com>
2024-05-03 13:17:15 +0530

    Initial commit by jagadeesh

---
+++ Z1-SpringBootProj-1/src/main/java/com/nt/Z1SpringBootProj1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z1SpringBootProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z1SpringBootProj1Application.class, args);
+	}
+
+}
