ff61fe11aa8b2fd2b316248d9dd341a40630d5bf
Satheesh <Satheesh@LAPTOP-R0FR3UCD>
2024-05-07 12:08:57 +0530

    updated hello method

--- Z1-SpringBootProj-1/src/main/java/com/nt/controller/EmployeeOptController.java
+++ Z1-SpringBootProj-1/src/main/java/com/nt/controller/EmployeeOptController.java
@@ -2,11 +2,14 @@ package com.nt.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeOptController {
 
+	
+	@GetMapping("/hello")
 	public ResponseEntity<String> hello()
 	{
 		return new ResponseEntity<String>("Hello world", HttpStatus.OK);
